432ced34b201220a981447a7f685ebd3f4d6581e
Amr Ragab <dolphinman1212@gmail.com>
2024-05-27 17:54:17 +0300

    Update Comment.js

--- models/Comment.js
+++ models/Comment.js
@@ -1,22 +1,21 @@
-export default (sequelize, DataTypes, comment) => {
-    const comment = sequelize.define('comment', {
-      content: DataTypes.STRING,
-      postId: {
-        type: DataTypes.INTEGER,
-        references: {
-          model: 'Posts',
-          key: 'id',
-        },
+export default (sequelize, DataTypes) => {
+  const Comment = sequelize.define('Comment', {
+    content: DataTypes.STRING,
+    postId: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Posts',
+        key: 'id',
       },
-      userId: {
-        type: DataTypes.INTEGER,
-        references: {
-          model: 'Users',
-          key: 'id',
-        },
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Users',
+        key: 'id',
       },
-    });
-    return comment;
-  };
-  
-  
+    },
+  });
+  return Comment;
+};
+
