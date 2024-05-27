39a7e0e2778f83ccbffde71b144f5b1fd71eb282
Amr Ragab <dolphinman1212@gmail.com>
2024-05-27 17:54:58 +0300

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,11 +1,12 @@
-export default (sequelize, DataTypes, user) => {
-    const user = sequelize.define('user', {
-      username: DataTypes.STRING,
-      email: {
-        type: DataTypes.STRING,
-        unique: true,
-      },
-      password: DataTypes.STRING,
-    });
-    return user;
-  };
+export default (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    username: DataTypes.STRING,
+    email: {
+      type: DataTypes.STRING,
+      unique: true,
+    },
+    password: DataTypes.STRING,
+  });
+  return User;
+};
+
