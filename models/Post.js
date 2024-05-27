881de718bb24de2a94ef217f15750d347024f891
Amr Ragab <dolphinman1212@gmail.com>
2024-05-27 17:54:36 +0300

    Update Post.js

--- models/Post.js
+++ models/Post.js
@@ -1,15 +1,15 @@
-export default (sequelize, DataTypes, post) => {
-    const post = sequelize.define('post', {
-      title: DataTypes.STRING,
-      content: DataTypes.STRING,
-      authorId: {
-        type: DataTypes.INTEGER,
-        references: {
-          model: 'Users',
-          key: 'id',
-        },
+export default (sequelize, DataTypes) => {
+  const Post = sequelize.define('Post', {
+    title: DataTypes.STRING,
+    content: DataTypes.STRING,
+    authorId: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Users',
+        key: 'id',
       },
-    });
-    return post;
-  };
-  
+    },
+  });
+  return Post;
+};
+
