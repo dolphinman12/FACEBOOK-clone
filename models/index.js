bab39284b69d1c44a72f6b0c58f387c97397c332
Amr Ragab <dolphinman1212@gmail.com>
2024-05-27 17:53:54 +0300

    Update index.js

--- models/index.js
+++ models/index.js
@@ -2,7 +2,7 @@ import { Sequelize } from 'sequelize';
 import user from './User.js';
 import post from './Post.js';
 import comment from './Comment.js';
-import config from '../config/config.json'assert { type: "json" };
+import config from '../config/config.json';
 
 const sequelize = new Sequelize(
   config.development.database,
@@ -15,21 +15,20 @@ const sequelize = new Sequelize(
 );
 
 const db = {
-    sequelize,
-    Sequelize,
-    user: user(sequelize, Sequelize),
-    post: post(sequelize, Sequelize),
-    comment: comment(sequelize, Sequelize),
-  };
-  
-  db.User.hasMany(db.Post, { foreignKey: 'authorId' });
-  db.Post.belongsTo(db.User, { foreignKey: 'authorId', as: 'author' });
-  
-  db.User.hasMany(db.Comment, { foreignKey: 'userId' });
-  db.Comment.belongsTo(db.User, { foreignKey: 'userId', as: 'user' });
-  
-  db.Post.hasMany(db.Comment, { foreignKey: 'postId' });
-  db.Comment.belongsTo(db.Post, { foreignKey: 'postId', as: 'post' });
-  
-export default db
-  
+  sequelize,
+  Sequelize,
+  User: user(sequelize, Sequelize),
+  Post: post(sequelize, Sequelize),
+  Comment: comment(sequelize, Sequelize),
+};
+
+db.User.hasMany(db.Post, { foreignKey: 'authorId' });
+db.Post.belongsTo(db.User, { foreignKey: 'authorId', as: 'author' });
+
+db.User.hasMany(db.Comment, { foreignKey: 'userId' });
+db.Comment.belongsTo(db.User, { foreignKey: 'userId', as: 'user' });
+
+db.Post.hasMany(db.Comment, { foreignKey: 'postId' });
+db.Comment.belongsTo(db.Post, { foreignKey: 'postId', as: 'post' });
+
+export default db;
